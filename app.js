77be7b6861ee26bbd7e28bd800443d8fb5152f59
Katya Juliet Lerner <katyajuliet22@gmail.com>
2022-05-11 16:30:26 -0700

    added website and files system

--- app.js
+++ app.js
@@ -1,21 +1,18 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
+const profileDataArgs = process.argv.slice(2);
+
+console.log(profileDataArgs);
 
 const [name, github] = profileDataArgs;
 
-const fs = require('fs');
+console.log(name, github);
 
-const generatePage = require('./src/page-template.js');
+const pageHTML = generatePage(name, github);
 
-fs.writeFile("index.html", generatePage(name, github), err => {
-    if(err) throw err;
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
 
-
-    console.log("Portfolio complete! Check out index.html to see the output!");
+  console.log('Portfolio complete! Check out index.html to see the output!');
 });
-// console.log(profileDataArgs);
-
-// const printProfileData = profileDataArr => {
-//     profileDataArr.forEach(profileItem => console.log(profileItem))
-// };
-
-// printProfileData(profileDataArgs);
